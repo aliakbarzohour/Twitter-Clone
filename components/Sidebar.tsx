ccd3fc86799b096403f102f0983b3fb42d169352
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-05-07 21:35:23 +0430

    end solid coding side bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,32 +1,37 @@
 import React from 'react'
 // Import Icons
 import {
-    BellIcon,
-    HashtagIcon,
-    BookmarkIcon,
-    CollectionIcon,
-    DotsCircleHorizontalIcon,
-    MailIcon,
-    UserIcon,
-    HomeIcon
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
 } from '@heroicons/react/outline'
 // Import Sidebar Row component
 import SidebarRow from './SidebarRow'
 
 function Sidebar() {
   return (
-    <>
-    {/* Twitter Logo */}
-    <img className='h-10 w-10' src="https://links.papareact.com/drq" alt="Twitter Logo" />
-    
-    <SidebarRow Icon={HomeIcon} title="Home"/>
-    <SidebarRow Icon={HashtagIcon} title="Explore"/>
-    <SidebarRow Icon={BellIcon} title="Notifications"/>
-    <SidebarRow Icon={MailIcon} title="Massages"/>
-    <SidebarRow Icon={BookmarkIcon} title="Bookmarks"/>
-    <SidebarRow Icon={CollectionIcon} title="Lists"/>
-    
-    </>
+    <div className='flex flex-col'>
+      {/* Twitter Logo */}
+      <img
+        className="h-10 w-10"
+        src="https://links.papareact.com/drq"
+        alt="Twitter Logo"
+      />
+      {/* icon's and title */}
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" /> 
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Massages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
   )
 }
 
